9cdadb2ca1f26e0e430e52a17827c5d03107cf38
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-02-24 00:53:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+// // . Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var guestArr = ["Noman", "Ali raza", "Mohsin", "Fazal"];
+console.log("I am inviting ".concat(guestArr.length, " people to dinner"));
