11a5ba8884e6f7c9716fa70a74667e977a501ea9
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 10:49:13 +0500

    Delete main.ts

--- main.ts
+++
@@ -1,5 +0,0 @@
-// // . Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-
-let guestArr : string[]=["Noman","Ali raza","Mohsin","Fazal"]
-
-console.log(`I am inviting ${guestArr.length} people to dinner`)
