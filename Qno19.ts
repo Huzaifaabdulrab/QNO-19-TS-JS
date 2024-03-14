f330941da1a5616a0932117d7e6c95c1f3293e18
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:40:23 +0500

    Add files via upload

---
+++ Qno19.ts
@@ -0,0 +1,4 @@
+// . Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let person_name : string[] = ["Huzaifa","Raza","Ali","Asad"];
+console.log(`I am  inivited ${person_name.length} people to Dinner `);
